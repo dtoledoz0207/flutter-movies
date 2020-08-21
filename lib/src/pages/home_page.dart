ac6be4420d40ceea9b2f2121e6dded520af5208a
David Toledo <dtoledoz0207@gmail.com>
2020-08-21 11:57:28 -0500

    add-flutter-swiper-to-dependencies-and-create-an-example-in-homePage

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('HOME!!!!!!!!!!!!'),
+      appBar: AppBar(
+        title: Text('Movies'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {}
+          )
+        ],
+        centerTitle: false,
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperCards()
+          ],
+        ),
       )
+    );
+  }
+
+  Widget _swiperCards() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 200.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context,int index){
+          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+        },
+        itemCount: 3,
+        itemWidth: 300.0,
+        layout: SwiperLayout.STACK,
+      ),
     );
   }
 }
