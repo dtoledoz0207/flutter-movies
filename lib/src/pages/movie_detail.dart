8473c1f4add8ee96f53f1c83bf7cb95932c2f351
David Toledo <dtoledoz0207@gmail.com>
2020-08-28 13:01:53 -0500

    add-SliverList-Widget-in-movieDetalPage-to-show-the-page-body

--- lib/src/pages/movie_detail.dart
+++ lib/src/pages/movie_detail.dart
@@ -11,7 +11,23 @@ class MovieDetail extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          _createAppBar(movie)
+          _createAppBar(movie),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10.0),
+                _posterTitle(context, movie),
+                _movieDescription(movie),
+                _movieDescription(movie),
+                _movieDescription(movie),
+                _movieDescription(movie),
+                _movieDescription(movie),
+                _movieDescription(movie),
+                _movieDescription(movie),
+                _movieDescription(movie)
+              ]
+            )
+          )
         ],
       )
     );
@@ -23,7 +39,7 @@ class MovieDetail extends StatelessWidget {
       backgroundColor: Colors.deepPurpleAccent,
       expandedHeight: 200.0,
       floating: false,
-      pinned: false,
+      pinned: true,
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
         title: Text(
@@ -36,6 +52,51 @@ class MovieDetail extends StatelessWidget {
           fadeInDuration: Duration(milliseconds: 200),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+
+  Widget _posterTitle(BuildContext context, Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image(
+              image: NetworkImage(movie.getPosterImg()),
+              height: 150.0,
+            ),
+          ),
+          SizedBox(width: 20.0),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(movie.title, style: Theme.of(context).textTheme.headline6, overflow: TextOverflow.ellipsis,),
+                Text(movie.originalTitle, style: Theme.of(context).textTheme.subtitle1, overflow: TextOverflow.ellipsis,),
+                SizedBox(height: 5.0),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star, color: Colors.yellow,),
+                    Text(movie.voteAverage.toString(), style: Theme.of(context).textTheme.subtitle1)
+                  ],
+                )
+              ],
+            )
+          )
+        ],
+      ),
+    );
+  }
+  
+
+  Widget _movieDescription(Movie movie) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Text(
+        movie.overview,
+        textAlign: TextAlign.justify,
       ),
     );
   }
