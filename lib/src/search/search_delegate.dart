150afa73f242073aecce93398cd077a432373ca5
David Toledo <dtoledoz0207@gmail.com>
2020-08-30 13:28:43 -0500

    create-searchDelegate-class

---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+      // AppBar action buttons
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildLeading(BuildContext context) {
+      // Icon to the left of the AppBar
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildResults(BuildContext context) {
+      // Create the results that we are going to show
+      throw UnimplementedError();
+    }
+  
+    @override
+    Widget buildSuggestions(BuildContext context) {
+    // These are the suggestions that appears when an user writes on the AppBar search
+    throw UnimplementedError();
+  }
+
+}
