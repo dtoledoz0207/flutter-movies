458072be185c0d2ce051af47d3a4edfb1777f522
David Toledo <dtoledoz0207@gmail.com>
2020-08-31 11:18:21 -0500

    create-ListView-builder-for-buildSuggestions-in-searchDelegate

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+
+  List movies = [
+    'Ironman',
+    'Superman',
+    'Batman',
+    'Wolverine',
+    'Wonderwoman',
+    'Spiderman',
+    'Daredevil'
+  ];
+
+  List lastMovies = [
+    'Spiderman',
+    'Daredevil'
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // AppBar action buttons
@@ -37,7 +53,21 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildSuggestions(BuildContext context) {
     // These are the suggestions that appears when an user writes on the AppBar search
-    return Container();
+
+    final suggestionList = (query.isEmpty) ?
+                            lastMovies :
+                            movies.where((movie) => movie.toLowerCase().startsWith(query.toLowerCase())).toList();
+
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(suggestionList[index]),
+          onTap: (){},
+        );
+      }
+    );
   }
 
 }
